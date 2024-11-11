9ad60f38b1db4eeecc4fef89b5bc8be84168e877
Katha Patel <patel.katha@northeastern.edu>
2024-11-10 18:06:28 -0600

    spring boot setup

--- src/main/java/edu/neu/csye6200/Driver.java
+++ src/main/java/edu/neu/csye6200/Driver.java
@@ -1,20 +1,20 @@
 package edu.neu.csye6200;
 
-import edu.neu.csye6200.Controller.Routes;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 
  * @author Rohan
  * 
  */
-
+@SpringBootApplication
 public class Driver {
 	public static void main(String[] args) {
 		System.out.println("============Main Execution Start===================\n\n");
+		SpringApplication.run(Driver.class, args);
 
          //Add your code in between these two print statements
-		Routes routes = new Routes();
-		routes.navigateTo("login");
 
 		System.out.println("\n\n============Main Execution End===================");
 	}
