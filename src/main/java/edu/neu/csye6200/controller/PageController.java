ffbfb36f7e8dc05f3960b706da4ec3988d3370a6
Nidhi Mehta <mehta.nidhi@northeastern.edu>
2024-11-18 14:30:19 -0500

    Created Login Frontend page and connected it with Login API

---
+++ src/main/java/edu/neu/csye6200/controller/PageController.java
@@ -0,0 +1,13 @@
+package edu.neu.csye6200.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+}
--- src/main/java/edu/neu/csye6200/service/LoginService.java
+++ src/main/java/edu/neu/csye6200/service/LoginService.java
@@ -1,7 +1,5 @@
 package edu.neu.csye6200.service;
 
-import edu.neu.csye6200.model.LoginDTO;
-
 public interface LoginService {
     boolean authenticate(String userName, String password);
 }
