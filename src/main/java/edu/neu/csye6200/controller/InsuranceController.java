7d2e02fd1aba46adb4660436d4304407418b839a
Katha Patel <patel.katha@northeastern.edu>
2024-11-29 01:12:03 -0600

    created http requests

--- src/main/java/edu/neu/csye6200/controller/InsuranceController.java
+++ src/main/java/edu/neu/csye6200/controller/InsuranceController.java
@@ -6,6 +6,8 @@ import edu.neu.csye6200.service.InsuranceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1")
 public class InsuranceController {
@@ -14,15 +16,35 @@ public class InsuranceController {
     private InsuranceService insuranceService;
 
 
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello World";
+    @GetMapping("/Insurance/{id}")
+    public InsuranceDTO getInsuranceById(@PathVariable int id) {
+        return insuranceService.getInsuranceById(id);
     }
+
+    @GetMapping("/Insurance")
+    public List<InsuranceDTO> getAllInsurance() {
+        return insuranceService.getAllInsurance();
+    }
+
+
 
     @PostMapping("/Insurance")
     public InsuranceDTO saveInsurance(@RequestBody InsuranceDTO insuranceDTO) {
         System.out.println("Received payload: " + insuranceDTO);
         return insuranceService.saveInsurance(insuranceDTO);
     }
+
+    @PutMapping("/Insurance/{id}")
+    public InsuranceDTO updateInsurance(@PathVariable int id, @RequestBody InsuranceDTO insuranceDTO) {
+        return insuranceService.updateInsurance(id, insuranceDTO);
+    }
+
+    @DeleteMapping("/Insurance/{id}")
+    public String deleteInsurance(@PathVariable int id) {
+        insuranceService.deleteInsurance(id);
+        return "Insurance deleted successfully!";
+    }
+
+
 
 }
--- src/main/java/edu/neu/csye6200/service/impl/InsuranceServiceImpl.java
+++ src/main/java/edu/neu/csye6200/service/impl/InsuranceServiceImpl.java
@@ -7,11 +7,85 @@ import edu.neu.csye6200.service.InsuranceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class InsuranceServiceImpl implements InsuranceService {
 
     @Autowired
     private InsuranceRepository insuranceRepository;
+
+    @Override
+    public List<InsuranceDTO> getAllInsurance() {
+        List<Insurance> insurances = insuranceRepository.findAll();
+
+        // Map the list of Insurance entities to a list of InsuranceDTOs
+        return insurances.stream().map(insurance -> {
+            InsuranceDTO dto = new InsuranceDTO();
+            dto.setPatientId(insurance.getPatientId());
+            dto.setInsuranceNumber(insurance.getInsuranceNumber());
+            dto.setInsuranceProvider(insurance.getInsuranceProvider());
+            dto.setInsuranceType(insurance.getInsuranceType());
+            dto.setInsuranceDate(new SimpleDateFormat("yyyy-MM-dd").format(insurance.getInsuranceDate()));
+            dto.setCoverageDetails(insurance.getCoverageDetails());
+            return dto;
+        }).collect(Collectors.toList());
+    }
+
+
+    @Override
+    public InsuranceDTO getInsuranceById(int id) {
+        Insurance insurance = insuranceRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Insurance not found with id: " + id));
+
+        InsuranceDTO dto = new InsuranceDTO();
+        dto.setPatientId(insurance.getPatientId());
+        dto.setInsuranceNumber(insurance.getInsuranceNumber());
+        dto.setInsuranceProvider(insurance.getInsuranceProvider());
+        dto.setInsuranceType(insurance.getInsuranceType());
+        dto.setInsuranceDate(new SimpleDateFormat("yyyy-MM-dd").format(insurance.getInsuranceDate()));
+        dto.setCoverageDetails(insurance.getCoverageDetails());
+        return dto;
+    }
+
+    @Override
+    public InsuranceDTO updateInsurance(int id, InsuranceDTO insuranceDTO) {
+        Insurance insurance = insuranceRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Insurance not found with id: " + id));
+
+        insurance.setInsuranceNumber(insuranceDTO.getInsuranceNumber());
+        insurance.setInsuranceProvider(insuranceDTO.getInsuranceProvider());
+        insurance.setInsuranceType(insuranceDTO.getInsuranceType());
+        try {
+            insurance.setInsuranceDate(new SimpleDateFormat("yyyy-MM-dd").parse(insuranceDTO.getInsuranceDate()));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        insurance.setCoverageDetails(insuranceDTO.getCoverageDetails());
+
+        Insurance updatedInsurance = insuranceRepository.save(insurance);
+
+        InsuranceDTO updatedDTO = new InsuranceDTO();
+        updatedDTO.setInsuranceNumber(updatedInsurance.getInsuranceNumber());
+        updatedDTO.setInsuranceProvider(updatedInsurance.getInsuranceProvider());
+        updatedDTO.setInsuranceType(updatedInsurance.getInsuranceType());
+        updatedDTO.setInsuranceDate(new SimpleDateFormat("yyyy-MM-dd").format(updatedInsurance.getInsuranceDate()));
+        updatedDTO.setCoverageDetails(updatedInsurance.getCoverageDetails());
+        return updatedDTO;
+    }
+
+
+    @Override
+    public void deleteInsurance(int id) {
+        Insurance insurance = insuranceRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Insurance not found with id: " + id));
+        insuranceRepository.delete(insurance);
+    }
+
+
 
     @Override
     public InsuranceDTO saveInsurance(InsuranceDTO insuranceDTO) {
