ec9f6dae59ef66da2d5652432f447d99f76ca4cd
Nidhi Mehta <mehta.nidhi@northeastern.edu>
2024-12-08 17:31:00 -0500

    Added redirection to doctor-dashboard

--- src/main/java/edu/neu/csye6200/controller/LoginController.java
+++ src/main/java/edu/neu/csye6200/controller/LoginController.java
@@ -1,5 +1,7 @@
 package edu.neu.csye6200.controller;
 
+import edu.neu.csye6200.entity.Staff;
+import edu.neu.csye6200.entity.Patient;
 import edu.neu.csye6200.model.LoginDTO;
 import edu.neu.csye6200.service.LoginService;
 import edu.neu.csye6200.utils.JwtUtil;
@@ -23,20 +25,32 @@ public class LoginController {
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> login(@RequestBody LoginDTO loginDTO) {
         boolean isPatientAuthenticated = loginService.authenticatePatient(loginDTO.getUsername(), loginDTO.getPassword());
-        boolean isStaffAuthenticated = loginService.authenticateStaff(loginDTO.getUsername(), loginDTO.getPassword());
+        Staff authenticatedStaff = loginService.getAuthenticatedStaff(loginDTO.getUsername(), loginDTO.getPassword());
 
         Map<String, String> response = new HashMap<>();
         if (isPatientAuthenticated) {
             String token = jwtUtil.generateToken(loginDTO.getUsername());
-//            return ResponseEntity.ok("/patient-dashboard");
+            Patient authenticatedPatient = loginService.getAuthenticatedPatient(loginDTO.getUsername(), loginDTO.getPassword());
+
             response.put("token", token);
             response.put("redirectUrl", "/patient-dashboard");
+            response.put("id", String.valueOf(authenticatedPatient.getPatientId()));
+            response.put("first_name", authenticatedPatient.getFirstName());
+            response.put("middle_name", authenticatedPatient.getMiddleName());
+            response.put("last_name", authenticatedPatient.getLastName());
             return ResponseEntity.ok(response);
-        } else if (isStaffAuthenticated) {
+        } else if (authenticatedStaff != null) {
             String token = jwtUtil.generateToken(loginDTO.getUsername());
-//            return ResponseEntity.ok("/staff-dashboard");
             response.put("token", token);
-            response.put("redirectUrl", "/staff-dashboard");
+            response.put("id", String.valueOf(authenticatedStaff.getStaffId()));
+            response.put("first_name", authenticatedStaff.getFirstName());
+            response.put("middle_name", authenticatedStaff.getMiddleName());
+            response.put("last_name", authenticatedStaff.getLastName());
+            if ("doctor".equalsIgnoreCase(authenticatedStaff.getRole())) {
+                response.put("redirectUrl", "/doctor-dashboard");
+            } else {
+                response.put("redirectUrl", "/staff-dashboard");
+            }
             return ResponseEntity.ok(response);
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
